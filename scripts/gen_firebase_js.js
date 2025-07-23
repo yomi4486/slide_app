99605abef432c0ad503aa11b98e48b0bb06949a8
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-07-24 04:44:06 +0900

    Define: dirname

--- scripts/gen_firebase_js.js
+++ scripts/gen_firebase_js.js
@@ -1,5 +1,10 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const templatePath = path.resolve(__dirname, "../utils/firebase.js.example");
 const outputPath = path.resolve(__dirname, "../utils/firebase.js");
